be27c723f108ab1ebd203e8042f33361b6346307
Nedelin222 <anedelin222@gmail.com>
2024-10-15 19:13:17 +0300

    Variables.py

--- Variables.py
+++ Variables.py
@@ -1,6 +1,8 @@
 nember_tasks = 12 # Количество выполненных ДЗ
 nember_hours = 1.5 # Количество затраченных часов
 Course_name = 'Python' # Название курса
-average_time = (nember_hours / nember_tasks )
+average_time = (nember_hours / nember_tasks ) # среднее время выполнения
 print("Курс:",Course_name,", всего задач:", nember_tasks,
-      ", затрачено часов:", nember_hours, ", среднее время выполнения:", average_time, ".")
+      ", затрачено часов:", nember_hours, ", среднее время выполнения:", average_time, ".") 
+
+
