9269e785ef4c18ea5ddbe3f7744997498ecbd0b2
Nedelin222 <anedelin222@gmail.com>
2024-10-15 19:05:10 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,6 @@
+nember_tasks = 12 # Количество выполненных ДЗ
+nember_hours = 1.5 # Количество затраченных часов
+Course_name = 'Python' # Название курса
+average_time = (nember_hours / nember_tasks )
+print("Курс:",Course_name,", всего задач:", nember_tasks,
+      ", затрачено часов:", nember_hours, ", среднее время выполнения:", average_time, ".")
